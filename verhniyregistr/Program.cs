3f578b80a66df68001de1e6e56677b117789350c
Matvey <PC@PC->
2020-06-04 14:43:16 +0300

    Добавьте файлы проекта.

---
+++ verhniyregistr/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace verhniyregistr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово нижнего регистра:");
+            string a = Console.ReadLine();
+            a.ToUpper();
+            Console.ReadKey();
+        }
+    }
+}
